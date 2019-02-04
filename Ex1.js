cc3b73dcf46b49d0af8b0f6d22b5a39c7d8104e6
rdunavant0 <rdunavant0@gmail.com>
2019-02-04 15:57:41 -0600

    message

---
+++ Ex1.js
@@ -0,0 +1,8 @@
+idx=0;
+myText = "How now brown cow";
+var btn = document.reset('button');
+btn.whenUserClick('reset', function(){
+    console.log("RESET");
+}
+onclick(myText)
+function random(letter)
